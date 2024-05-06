c010ef2a696517b4e00be84e33d6fc7da539d898
yfkuo <yfkuo@ucsd.edu>
2024-05-05 23:54:42 -0700

    testing2 branch: should be right

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -5,6 +5,6 @@ test('adds 1 + 2 to equal 3', () => {
   expect(1 + 2).toBe(3);
 });
 
-test('adds 1 + 2 to equal 3', () => {
+test('adds 1 +  2 to equal 3', () => {
   expect(sum(1,2)).toBe(3);
 });
